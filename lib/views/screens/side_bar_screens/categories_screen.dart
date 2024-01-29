204ef03505ebf2bb44061b2528283e560d5acf03
한요한 <yoyoh0tz@han-yohans-MacBook-Air.local>
2024-01-29 12:05:55 +0900

    categories_screen.dart(storage,firestore, realtime builder)

--- lib/views/screens/side_bar_screens/categories_screen.dart
+++ lib/views/screens/side_bar_screens/categories_screen.dart
@@ -1,21 +1,193 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:web_admin_page/views/screens/side_bar_screens/widgets/category_widget.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   static const String routeName = '\CategoriesScreen';
 
   @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  late String categoryName;
+
+  dynamic _image;
+  String? fileName;
+
+  _pickImage() async {
+    FilePickerResult? result = await FilePicker.platform
+        .pickFiles(allowMultiple: false, type: FileType.image);
+
+    if (result != null) {
+      setState(() {
+        _image = result.files.first.bytes;
+        fileName = result.files.first.name;
+      });
+    }
+  }
+
+  _uploadCategoryBannerToStorage(dynamic image) async {
+    Reference ref = _storage.ref().child('CategoryImages').child(fileName!);
+
+    UploadTask uploadTask = ref.putData(image);
+
+    TaskSnapshot snapshot = await uploadTask;
+    String downloadURL = await snapshot.ref.getDownloadURL();
+    return downloadURL;
+  }
+
+  uploadCategory() async {
+    EasyLoading.show();
+    if (_formKey.currentState!.validate()) {
+      String imageURL = await _uploadCategoryBannerToStorage(_image);
+      await _firestore.collection('categories').doc(fileName).set({
+        'image': imageURL,
+        'categoryName': categoryName,
+      }).whenComplete(() {
+        EasyLoading.dismiss();
+        setState(() {
+          _image = null;
+          _formKey.currentState!.reset();
+        });
+      });
+    } else {
+      print("bad");
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        alignment: Alignment.topLeft,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Categories',
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            fontSize: 36,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Categories',
+              style: TextStyle(
+                fontWeight: FontWeight.w700,
+                fontSize: 36,
+              ),
+            ),
           ),
-        ),
+          const Divider(
+            color: Colors.grey,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(14.0),
+                child: Column(
+                  children: [
+                    Container(
+                      height: 140,
+                      width: 140,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        border: Border.all(
+                          color: Colors.grey.shade500,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: _image != null
+                          ? Image.memory(
+                              _image,
+                              fit: BoxFit.cover,
+                            )
+                          : const Center(
+                              child: Text(
+                                "Category",
+                              ),
+                            ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.yellow.shade900,
+                      ),
+                      onPressed: () {
+                        _pickImage();
+                      },
+                      child: Text(
+                        'Upload Image',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Flexible(
+                child: SizedBox(
+                  width: 180,
+                  child: TextFormField(
+                    onChanged: (value) {
+                      categoryName = value;
+                    },
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Category Name Must Not Be Empty';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(
+                      labelText: 'Enter Category Name',
+                      hintText: 'Enter Category Name',
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.yellow.shade900,
+                ),
+                onPressed: () {
+                  uploadCategory();
+                },
+                child: Text(
+                  'Save',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Divider(
+              color: Colors.grey,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              alignment: Alignment.topLeft,
+              child: Text(
+                "Categories",
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          CategoryWidget(),
+        ],
       ),
     );
   }
---
+++ lib/views/screens/side_bar_screens/widgets/category_widget.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class CategoryWidget extends StatelessWidget {
+  CategoryWidget({super.key});
+
+  final Stream<QuerySnapshot> _categoriesStream =
+      FirebaseFirestore.instance.collection('categories').snapshots();
+
+  @override
+  Widget build(BuildContext context) {
+    // flutter real-time streambuilder: https://firebase.flutter.dev/docs/firestore/usage/#one-time-read
+    return StreamBuilder<QuerySnapshot>(
+      stream: _categoriesStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.cyan,
+            ),
+          );
+        }
+
+        return GridView.builder(
+          shrinkWrap: true,
+          itemCount: snapshot.data!.size,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 6,
+            mainAxisSpacing: 8,
+            crossAxisSpacing: 8,
+          ),
+          itemBuilder: (context, index) {
+            final categoryData = snapshot.data!.docs[index];
+            return Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                  width: 100,
+                  child: Image.network(
+                    categoryData['image'],
+                  ),
+                ),
+                Text(
+                  categoryData['categoryName'],
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
